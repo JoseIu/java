f3c509b7f54fbac95b3eb428938412a47be9fb42
Joselu <joseluissgz97@gmail.com>
2024-06-30 19:45:00 +0200

    add: App para ver las clases y herenmcias

---
+++ src/clases_objetos/herencia3_ejercicio/App.java
@@ -0,0 +1,15 @@
+package clases_objetos.herencia3_ejercicio;
+
+public class App {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        System.out.println(employee);
+
+        employee = new Employee("Sara","23235235F","4367357",2500,"Teacher");
+
+        System.out.println(employee);
+
+
+
+    }
+}
